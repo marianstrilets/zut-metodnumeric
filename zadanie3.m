5aa61d6217652b6f034dd5bea899a069a152d9e5
marianstrilets <marianstrilets@gmail.com>
2023-01-10 09:51:10 +0100

    Update zadanie3.m

--- zadanie3.m
+++ zadanie3.m
@@ -1,8 +1,9 @@
 %zadanie 3
-%Rozwiąz rownanie x^3 + 2*x^2 - 100 = 0 z dokładnoscią 0.005
+%MATLAB Rozwiąz rownanie x^3 + 2*x^2 - 100 = 0 z dokładnoscią 0.005
 %odpowiedzi: 1;
 
 
 h = 0.005;
 
 fzero('(x^3) + 2*(x^2) - 100', -100)
+
