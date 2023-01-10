12ff8907d3c7ff846bcd407239cdb63073e17d0f
marianstrilets <marianstrilets@gmail.com>
2023-01-10 09:51:12 +0100

    Update zadanie5.m

--- zadanie5.m
+++ zadanie5.m
@@ -1,16 +1,29 @@
 %zadanie 5
+%odpowiedzi: 3;
 
-x = [-2 1 3 7]
-y = [0 2 3 11]
+%Funkcja y=f(x) okreśona jest w 4 punktach (węzlach)
+%x = [-2 1 3 7]
+%y = [0 2 3 11]
+%podać przybliżonę wartośći funkcji dla x=5 stosując interpolacje
+%   a)liniowa
+%   b)spline
+%   c)wielomianu
+
+x = [-2 1 3 7];
+y = [0 2 3 11];
 
 %f(5) stosujac interolacje liniowa
 
 
 %interp1(x, y, 5, 'linear') = 7
-interp1(x, y, 5, 'linear') 
+a_linear = interp1(x, y, 5, 'linear');
+disp('a)liniowa: ');
+disp(a_linear);
 
 %interp1(x, y, 5, 'spline') = 5.4963
-interp1(x, y, 5, 'spline') 
+b_spline = interp1(x, y, 5, 'spline');
+disp('b)spline: ');
+disp(b_spline);
 
 A = polyfit(x,y,3) %odpowiedz: 5.4963
 % 3 bo welomian 3 stopnia(4 wezely)
