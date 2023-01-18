0f041954c9d787ffd62678e6c120279d031dac4b
marianstrilets <marianstrilets@gmail.com>
2023-01-18 15:51:02 +0100

    Delete z_3.m

--- z_3.m
+++
@@ -1,9 +0,0 @@
-%Rozwiąz rownanie x^3 + 2*x^2 - 100 = 0 z dokładnoscią 0.005
-%odpowiedzi: 1;
-
-x1 = fzero('(x^3) + (2*x^2) - 100', 0.005);
-disp(x1);
-
-
-%x2 = fzero(@(x) x^3 + 2*x^2 - 100, [0 5]);
-%disp(x2);
