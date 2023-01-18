ee04cd9e127c2fbae17b52e9ca495055a925fabd
marianstrilets <marianstrilets@gmail.com>
2023-01-18 15:50:41 +0100

    Create zadanie3.m

---
+++ Kolokwium01/zadanie3.m
@@ -0,0 +1,11 @@
+%zadanie 3
+%MATLAB Rozwiąz rownanie x^3 + 2*x^2 - 100 = 0 z dokładnoscią 0.005
+%odpowiedzi: 1;
+
+
+h = 0.005;
+
+fzero('(x^3) + 2*(x^2) - 100', -100)
+
+matlab
+Rozwiąz rownanie lnx + x^2 - 5 = 0
